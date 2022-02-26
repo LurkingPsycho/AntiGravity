fb1c08d0cce0dcc00417edcba5afcfc24d6cdcc8
LurkingPsycho <34584533+LurkingPsycho@users.noreply.github.com>
2022-02-26 10:03:54 +0000

    Update AntiGravity.java

--- src/mc/gravity/moon/AntiGravity.java
+++ src/mc/gravity/moon/AntiGravity.java
@@ -1,49 +1,35 @@
 package mc.gravity.moon;
 
-import java.io.InputStreamReader;
-
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandExecutor;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import mc.gravity.moon.commands.AntiGravityToggle;
-import mc.gravity.moon.commands.agReload;
+import mc.gravity.moon.config.DataManager;
 import mc.gravity.moon.listeners.GravityToggle;
-import mc.gravity.moon.listeners.WorldChange;
 import mc.gravity.moon.utils.ChatUtilities;
 
 public class AntiGravity extends JavaPlugin implements CommandExecutor {
-		
+	
+	public DataManager data;
+	
 	public void onEnable() {
-		Bukkit.broadcastMessage(ChatUtilities.msg("&aAntiGravity &64.5 &ahas now been enabled!"));
-		onConfig(getConfig());
+		Bukkit.broadcastMessage(ChatUtilities.msg("&aAntiGravity &65.0 &ahas now been enabled!"));
+		data = new DataManager(this);
 		onCommand();
 		onLoad(Bukkit.getServer().getPluginManager());
-		reloadConfig();
 	}
-	
-	
-	public void onConfig(FileConfiguration config) {
-		saveResource("config.yml", false);
-		config = YamlConfiguration.loadConfiguration(new InputStreamReader(getResource("config.yml")));
-	}
-	
+
 	public void onCommand() {
-		getCommand("ag").setExecutor(new AntiGravityToggle(this));
+		getCommand("ag").setExecutor(new AntiGravityToggle(data));
 		
 	}
 	
 	public void onLoad(PluginManager pm) {
-		pm.registerEvents(new GravityToggle(this), this);
-		pm.registerEvents(new WorldChange(this), this);
+		pm.registerEvents(new GravityToggle(data), this);
+	
 	}
 	
-	public void reload() {
-		reloadConfig();
-		
-	}
 
 }
