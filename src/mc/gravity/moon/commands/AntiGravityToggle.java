a5e8cff7bdb311c74c80ccdd38fd029e24acb221
LurkingPsycho <34584533+LurkingPsycho@users.noreply.github.com>
2022-02-26 09:58:21 +0000

    Update AntiGravityToggle.java

--- src/mc/gravity/moon/commands/AntiGravityToggle.java
+++ src/mc/gravity/moon/commands/AntiGravityToggle.java
@@ -12,30 +12,33 @@ import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 import mc.gravity.moon.AntiGravity;
-
+import mc.gravity.moon.config.DataManager;
 import mc.gravity.moon.utils.ChatUtilities;
 
 public class AntiGravityToggle implements CommandExecutor {
 	
-	public AntiGravity ag;
-	FileConfiguration config;
+	public DataManager data;
+	
 	public static ArrayList<String> toggle = new ArrayList<String>();
 	
-	public AntiGravityToggle(AntiGravity ag) {
-		this.config = ag.getConfig();
-		this.ag = ag;
+	public AntiGravityToggle(DataManager data) {
+		this.data = data;
 	}
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		
 		if(!(sender instanceof Player)) {
-			ag.reloadConfig();
+			data.reloadConfig();
 			sender.sendMessage(ChatUtilities.msg("&aAntiGravity has been reloaded!"));
 		}
 		
 		if(sender instanceof Player) {
 			if(cmd.getName().equals("ag")) {
 				Player p = (Player) sender;
+				if(args.length == 0) {
+					p.sendMessage(ChatUtilities.msg("&a/ag toggle"));
+					return false;
+				}
 				if(args[0].equalsIgnoreCase("toggle")) {
 					if(!p.hasPermission("antigravity.use")) {
 						p.sendMessage(ChatUtilities.msg("&cSorry, you do not have permission"));
@@ -44,8 +47,8 @@ public class AntiGravityToggle implements CommandExecutor {
 							if(!toggle.contains(p.getName())) {
 								toggle.add(p.getName());
 								p.sendMessage(ChatUtilities.msg("&bAntiGravity &aEnabled!"));
-							    p.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, config.getInt("JumpTime"), config.getInt("JumpHeight"), false, false));
-								p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING, config.getInt("SlowFallingTime"), config.getInt("SlowFalling"), false, false));
+							    p.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, data.getConfig().getInt("JumpTime"), data.getConfig().getInt("JumpHeight"), false, false));
+								p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING, data.getConfig().getInt("SlowFallingTime"), data.getConfig().getInt("SlowFalling"), false, false));
 							} else {
 								toggle.remove(p.getName());
 								p.sendMessage(ChatUtilities.msg("&bAntiGravity &cDisabled!"));
@@ -57,6 +60,16 @@ public class AntiGravityToggle implements CommandExecutor {
 						}
 						
 					}
+				}
+				if(args[0].equalsIgnoreCase("reload")) {
+					if(!p.hasPermission("antigravity.reload")) {
+						p.sendMessage(ChatUtilities.msg("&cSorry, you do not have permission"));
+					} else {
+						data.reloadConfig();
+						p.sendMessage(ChatUtilities.msg("&aAntiGravity has been reloaded! will take affect when 'ReEnabling AntiGravity toggle'!"));
+						return false;
+					}
+				
 				}
 				
 			}
