c50a89fa4e0362385347af29b306090347beebbd
LurkingPsycho <34584533+LurkingPsycho@users.noreply.github.com>
2022-02-26 09:58:55 +0000

    Update GravityToggle.java

--- src/mc/gravity/moon/listeners/GravityToggle.java
+++ src/mc/gravity/moon/listeners/GravityToggle.java
@@ -3,11 +3,9 @@ package mc.gravity.moon.listeners;
 import java.util.ArrayList;
 
 import org.bukkit.Material;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -17,27 +15,25 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
-import com.sk89q.worldguard.WorldGuard;
-
 import mc.gravity.moon.AntiGravity;
-import mc.gravity.moon.commands.AntiGravityToggle;
+import mc.gravity.moon.config.DataManager;
 import mc.gravity.moon.utils.ChatUtilities;
 
 public class GravityToggle implements Listener {
 	
-	FileConfiguration config;
+	public DataManager data;
 
 	public static ArrayList<String> toggle = new ArrayList<String>();
 	
-	public GravityToggle(AntiGravity ag) {
-		this.config = ag.getConfig();
+	public GravityToggle(DataManager data) {
+		this.data = data;
 	}
 	
 	public void giveItem(Player p) {
 		
-		int slot = config.getInt("item-slot");
+		int slot = data.getConfig().getInt("item-slot");
 		
-		ItemStack item = new ItemStack(Material.matchMaterial(config.getString("antigravity-item")), 1);
+		ItemStack item = new ItemStack(Material.matchMaterial(data.getConfig().getString("antigravity-item")), 1);
 		ItemMeta meta = item.getItemMeta();
 		
 		meta.setDisplayName(ChatUtilities.msg("&a&lAntiGravity Toggle"));
@@ -68,13 +64,13 @@ public class GravityToggle implements Listener {
 	public void onGravToggle(PlayerInteractEvent e) {
 
 		try {
-			if(e.getItem().getType() == Material.matchMaterial(config.getString("antigravity-item"))) {
+			if(e.getItem().getType() == Material.matchMaterial(data.getConfig().getString("antigravity-item"))) {
 				
 				if(!toggle.contains(e.getPlayer().getName())) {
 					toggle.add(e.getPlayer().getName());
 					e.getPlayer().sendMessage(ChatUtilities.msg("&bAntiGravity &aEnabled!"));
-				    e.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.JUMP, config.getInt("JumpTime"), config.getInt("JumpHeight"), false, false));
-					e.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING, config.getInt("SlowFallingTime"), config.getInt("SlowFalling"), false, false));
+				    e.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.JUMP, data.getConfig().getInt("JumpTime"), data.getConfig().getInt("JumpHeight"), false, false));
+					e.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING, data.getConfig().getInt("SlowFallingTime"), data.getConfig().getInt("SlowFalling"), false, false));
 				} else {
 					toggle.remove(e.getPlayer().getName());
 					e.getPlayer().sendMessage(ChatUtilities.msg("&bAntiGravity &cDisabled!"));
